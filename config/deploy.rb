65846d3a78516a91d80010dd1f082f4a4fa37d37
mohan <mohan589@gmail.com>
2015-04-08 11:22:23 +0530

    added password

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :releases_path, File.join(deploy_to)
 # set :log_level, :debug
 
 # Default value for :pty is false
-set :pty, true
+# set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
